acb3fef3761cd05e84b835b70f052e7a50402739
aimeos <aimeos@aimeos.org>
2026-09-02 08:57:16 +0000

    Added cache:clear permission to publisher role

--- config/cms.php
+++ config/cms.php
@@ -27,7 +27,7 @@ return [
     'roles' => [
         'admin' => ['*'],
         'viewer' => ['page:view', 'element:view', 'file:view'],
-        'publisher' => ['page:*', 'element:*', 'file:*', 'audio:*', 'image:*', 'text:*', 'page:config'],
+        'publisher' => ['page:*', 'element:*', 'file:*', 'audio:*', 'image:*', 'text:*', 'page:config', 'cache:clear'],
         'editor' => ['publisher', '!*:publish', '!*:purge', '!page:access'],
     ],
 
